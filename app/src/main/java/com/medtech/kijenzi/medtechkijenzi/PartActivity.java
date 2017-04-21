17fd32393580cba2096eeb0527cb205d3774d956
Tony Powell <adpowell@mtu.edu>
2017-04-21 10:54:17 -0400

    Add partname on click to view

--- app/src/main/java/com/medtech/kijenzi/medtechkijenzi/PartActivity.java
+++ app/src/main/java/com/medtech/kijenzi/medtechkijenzi/PartActivity.java
@@ -3,6 +3,8 @@ package com.medtech.kijenzi.medtechkijenzi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PartActivity extends AppCompatActivity {
@@ -12,8 +14,13 @@ public class PartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part);
         Intent intent = getIntent();
-        Integer thumbId = intent.getIntExtra("THUMB_ID", -1);
-        Toast thumbToast = Toast.makeText(this, "Thumb id loaded " + thumbId, Toast.LENGTH_LONG);
-        thumbToast.show();
+
+        // Get part name and display it in the textview
+        TextView partName = (TextView) findViewById(R.id.PartNameText);
+        partName.setText(intent.getCharSequenceExtra("PART_NAME"));
+
+//        Integer thumbId = intent.getIntExtra("THUMB_ID", -1);
+//        Toast thumbToast = Toast.makeText(this, "Thumb id loaded " + thumbId, Toast.LENGTH_LONG);
+//        thumbToast.show();
     }
 }
